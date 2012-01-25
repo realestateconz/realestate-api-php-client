7e68a22c2c0516fb9f3ad3d4a0924945bedab6ab
warezthebeef <warezthebeef@gmail.com>
2012-01-25 15:12:18 +1300

    Adde agent enquire method

---
+++ lib/Api/Method/Listing/Enquire.php
@@ -0,0 +1,18 @@
+<?php
+
+class RealestateCoNz_Api_Method_Listing_Enquire extends RealestateCoNz_Api_Method {
+
+    protected $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+
+    }
+
+    public function getUrl()
+    {
+        return '/listings/' . $this->id '/agent-enquiry/';
+    }
+}
+
