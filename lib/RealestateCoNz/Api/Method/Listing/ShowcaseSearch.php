2dba8c57d6fcec434fa1b5a0391c8dc6771ab469
Inyoung <inyoung@realestate.co.nz>
2014-03-17 13:04:38 +1300

    new showcase search api method - ticket # 3892

---
+++ lib/RealestateCoNz/Api/Method/Listing/ShowcaseSearch.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author inyoung@realestate.co.nz
+ */
+
+class RealestateCoNz_Api_Method_Listing_ShowcaseSearch extends RealestateCoNz_Api_Method 
+{
+
+    public function getUrl()
+    {
+        return '/showcase/';
+    }
+}
+
